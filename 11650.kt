d11e2761f752012e7e00b9e9f5335cd65b759f07
ese111 <feung94@gmail.com>
2022-07-05 22:53:41 +0900

    11650 좌표 정렬하기 해결
    해결

---
+++ 11650.kt
@@ -0,0 +1,38 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
+import kotlin.collections.ArrayList
+
+private fun setDp(list: List<Pair<Int, Int>>): StringBuilder {
+    val arr = list.sortedWith { d1, d2 ->
+        if (d1.first == d2.first) {
+            d1.second - d2.second
+        } else {
+            d1.first - d2.first
+        }
+    }
+    val sb = StringBuilder()
+    for (i in 0 until arr.size) {
+        sb.append(arr[i].first)
+        sb.append(" ")
+        sb.append(arr[i].second)
+        sb.append("\n")
+    }
+    return sb
+}
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n = br.readLine().toInt()
+    val list = ArrayList<Pair<Int, Int>>()
+    for (i in 0 until n) {
+        val (x,y) = br.readLine().split(" ").map { it.toInt() }
+        list.add(Pair(x,y))
+    }
+
+    val ans = setDp(list)
+
+    println(ans)
+}
+
+
